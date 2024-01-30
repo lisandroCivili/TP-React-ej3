d08cf73bc18e7a854df72d0fe8d495f0dd6a1998
Lisandro Civili <lisandrocivili13@gmail.com>
2024-01-30 20:35:57 -0300

    Agrego funcion para q al hacer click se agregue un mensaje

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -1,8 +1,18 @@
-function App() {
+import { useState } from "react"
+
+function HelloWorld() {
+  const [message, setMessage] = useState(""); 
+
+  const agregarMensaje = () =>{
+    setMessage(message + "(from changed state)!");
+  }
 
   return (
-    <h1 className="text-center display-3">Hello world!</h1>
+    <>
+      <h1 className="display-3 ms-3">Hello world {message}</h1>
+      <button className="btn btn-primary ms-3" onClick={agregarMensaje}>Click me</button>
+    </>
   )
 }
 
-export default App
+export default HelloWorld
